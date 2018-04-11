030258035be5f101735cab57b00ad21fc52cf655
Puyan Wei <puyanwei@gmail.com>
2018-04-11 11:39:04 +0100

    quoteGrid loops through the quotes array in App.vue which is passed down as a prop

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="container">
+        <quote-grid :quotes="quotes"></quote-grid>
     </div>
 </template>
 
 <script>
+import quoteGrid from './components/QuoteGrid.vue';
+
 export default {
     data: function() {
         return {
             quotes: ['The default quote'],
             maxQuotes: 10,
         };
+    },
+    components: {
+        quoteGrid,
     },
 };
 </script>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="row">
+      <singleQuote v-for="quote in quotes">{{ quote }}</singleQuote>
+  </div>
+</template>
+
+<script>
+import singleQuote from './SingleQuote.vue';
+
+export default {
+    props: ['quotes'],
+    components: {
+        singleQuote,
+    },
+};
+</script>
+
+<style lang="css">
+</style>
