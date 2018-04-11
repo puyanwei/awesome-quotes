1d2db122a09e4bef57e83da458fc87ba494ec478
Puyan Wei <puyanwei@gmail.com>
2018-04-11 11:17:50 +0100

    setup styling and div structure for SingleQuote.vue

--- src/SingleQuote.vue
+++ src/SingleQuote.vue
@@ -1,10 +1,29 @@
 <template lang="html">
-  
+  <div class="col-sm6 col-md-4 col-lg-3">
+      <div class=".panel.panel-default">
+          <div class="panel-body quote">
+              <slot></slot>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="css">
+<style>
+.panel-body {
+    font-family: 'Arizonia', cursive;
+    font-size: 24px;
+    color: #6e6e6e;
+}
+
+.quote {
+    cursor: pointer;
+}
+
+.quote:hover {
+    background-color: #ffe2e2;
+}
 </style>
