86cd170bcfd5e5a76bd40334732e7045ce7c477a
Puyan Wei <puyanwei@gmail.com>
2018-04-11 15:05:33 +0100

    added props to Header.vue, and the calculations needed change the progress bar dynamically

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,14 @@
         <div class="col-sm-12">
             <h3>Quotes Added</h3>
             <div class="progress">
-                <div class="progress-bar" role="progress-bar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                <div
+                class="progress-bar"
+                role="progress-bar"
+                aria-valuenow="60"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
+                    {{ quoteCount }} / {{ maxQuotes }}
                 </div>
             </div>
         </div>
@@ -11,7 +18,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+    props: ['quoteCount', 'maxQuotes'],
+};
 </script>
 
 <style lang="css">
