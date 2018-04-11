8a000f292b04755c3c47ad3fde45a0ef9295e2e7
Puyan Wei <puyanwei@gmail.com>
2018-04-11 11:47:56 +0100

    added NewQuote.vue with styling and bootstrap structuring. Added form with text box and button

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div >
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            quote: '',
+        };
+    },
+    methods: {
+        createNew() {},
+    },
+};
+</script>
+
+<style lang="css">
+</style>
